bc7bf37ba00e38fb0bcd5a28e1fe1cacce0e54ea
wachin <wachin.id@gmail.com>
2021-09-28 16:09:20 -0500

    update

--- .01-Lanzador-push.sh
+++ .01-Lanzador-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-konsole -e "./.02-autosave-script.sh" &
+konsole -e "./.02-autosave-script.sh"
 
 
--- .03-Lanzador-pull.sh
+++ .03-Lanzador-pull.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-xfce4-terminal -e "./04-script-pull.sh" &
+xfce4-terminal -e "./.04-script-pull.sh"
 
-04-script-pull.sh
